9e9f0cd00b72aa09576fc40ddc2e9e6c68efcc61
Leo Broukhis <leobru@users.noreply.github.com>
2021-12-13 16:36:06 -0800

    Create pastime.pas
    A procedure printing the amount of time elapsed since last invocation.

---
+++ pastime.pas
@@ -0,0 +1,17 @@
+(*=p-,t-,s8,y+*)program pastime(output, (*=s1*)timeold);
+var timeold : real;
+function pascodad(a, b: integer): char; external;
+procedure time;
+var l2var1z, l2var2z:real; l2var3z: char;
+_(
+    l2var1z := ord(PASCODAD(0, 630004C));
+    l2var1z := l2var1z / 50;
+    l2var3z := PASCODAD(0, 630000C);
+    l2var2z := ;
+  writeln(' RUNT=', l2var1z:0:22,
+          '.REST=', l2var2z:0:22,
+          '.DIFF=', l2var1z-TIMEOLD:0:22);
+  TIMEOLD := l2var1z;
+
+_);
+_( time _).data timeold:=0.0 end
